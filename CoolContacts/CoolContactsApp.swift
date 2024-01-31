fe87fd8139c7ccc184152d145d5247894dd51e12
stixlee <michael.lee.1983@iclllud.com>
2024-01-31 15:23:01 -0700

    Initial Commit

---
+++ CoolContacts/CoolContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoolContactsApp.swift
+//  CoolContacts
+//
+//  Created by Michael Lee on 1/31/24.
+//
+
+import SwiftUI
+
+@main
+struct CoolContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
